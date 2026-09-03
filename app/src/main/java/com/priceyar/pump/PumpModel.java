61a420cb61c99b77bdf179ccd7fc2cd89fea9536
Mahyar1393 <aliabed13662203@gmail.com>
2026-09-03 10:35:10 +0330

    Update PumpModel.java

--- app/src/main/java/com/priceyar/pump/PumpModel.java
+++ app/src/main/java/com/priceyar/pump/PumpModel.java
@@ -1,1 +1,13 @@
 package com.priceyar.pump;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "pump_models")
+public class PumpModel {
+    @PrimaryKey(autoGenerate = true)
+    public long id;
+
+    public String code;
+    public String name;
+    public double profitPercent;
+}
