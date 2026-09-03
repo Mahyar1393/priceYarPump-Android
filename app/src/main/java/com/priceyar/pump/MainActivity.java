d5ad187fb896a41bbb00a24269eb10c228898662
Mahyar1393 <aliabed13662203@gmail.com>
2026-09-03 10:57:07 +0330

    Update MainActivity.java

--- app/src/main/java/com/priceyar/pump/MainActivity.java
+++ app/src/main/java/com/priceyar/pump/MainActivity.java
@@ -1,1 +1,16 @@
 package com.priceyar.pump;
+import android.app.Activity;
+import android.os.Bundle;
+import android.print.PrintAttributes;
+import android.print.PrintManager;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.*;
+import android.view.View;
+
+import androidx.room.Room;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
