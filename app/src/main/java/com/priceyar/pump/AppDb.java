872fab70066f5f4ad18daf0e4cf0e020380a612e
Mahyar1393 <aliabed13662203@gmail.com>
2026-09-03 10:52:42 +0330

    Update AppDb.java

--- app/src/main/java/com/priceyar/pump/AppDb.java
+++ app/src/main/java/com/priceyar/pump/AppDb.java
@@ -1,1 +1,13 @@
 package com.priceyar.pump;
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(
+    entities = {Material.class, PumpModel.class, PumpItem.class},
+    version = 1,
+    exportSchema = false
+)
+public abstract class AppDb extends RoomDatabase {
+    public abstract MaterialDao materialDao();
+    public abstract PumpDao pumpDao();
+}
