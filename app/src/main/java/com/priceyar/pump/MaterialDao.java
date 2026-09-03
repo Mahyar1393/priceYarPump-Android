432970a7976b5fc8ec21c049e6f1468255c78e49
Mahyar1393 <aliabed13662203@gmail.com>
2026-09-03 10:46:34 +0330

    Update MaterialDao.java

--- app/src/main/java/com/priceyar/pump/MaterialDao.java
+++ app/src/main/java/com/priceyar/pump/MaterialDao.java
@@ -1,1 +1,18 @@
 package com.priceyar.pump;
+import androidx.room.*;
+import java.util.List;
+
+@Dao
+public interface MaterialDao {
+    @Query("SELECT * FROM materials ORDER BY id")
+    List<Material> getAll();
+
+    @Insert
+    long insert(Material m);
+
+    @Update
+    void update(Material m);
+
+    @Delete
+    void delete(Material m);
+}
