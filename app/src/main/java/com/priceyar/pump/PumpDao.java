dce65c3c2dbf4b537ea7b15480a71317a50494a0
Mahyar1393 <aliabed13662203@gmail.com>
2026-09-03 10:49:43 +0330

    Update PumpDao.java

--- app/src/main/java/com/priceyar/pump/PumpDao.java
+++ app/src/main/java/com/priceyar/pump/PumpDao.java
@@ -1,1 +1,33 @@
 package com.priceyar.pump;
+import androidx.room.*;
+import java.util.List;
+
+@Dao
+public interface PumpDao {
+    @Query("SELECT * FROM pump_models ORDER BY id")
+    List<PumpModel> getAll();
+
+    @Insert
+    long insert(PumpModel p);
+
+    @Update
+    void update(PumpModel p);
+
+    @Delete
+    void delete(PumpModel p);
+
+    @Query("SELECT * FROM pump_items WHERE pumpId=:pumpId ORDER BY id")
+    List<PumpItem> getItems(long pumpId);
+
+    @Insert
+    long insertItem(PumpItem item);
+
+    @Update
+    void updateItem(PumpItem item);
+
+    @Delete
+    void deleteItem(PumpItem item);
+
+    @Query("DELETE FROM pump_items WHERE pumpId=:pumpId")
+    void deleteItems(long pumpId);
+}
